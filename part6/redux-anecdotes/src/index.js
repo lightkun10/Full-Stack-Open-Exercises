6326f3c31baeaf508c9ee5057f92ed1a04e45aa2
pandu <lifeisubw@gmail.com>
2021-01-26 10:12:47 +0700

    Finish 6.15 Anecdotes and the backend, step3

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import App from './App'
 import store from './store';
-
-// const store = createStore(reducer);
-
-// console.log(store.getState());
+import App from './App'
 
 ReactDOM.render(
   <Provider store={store}>
--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import anecdoteReducer, { initializeAnecdotes } from './reducers/anecdoteReducer';
 import notificationReducer from './reducers/notificationReducer';
@@ -12,7 +13,10 @@ const reducer = combineReducers({
 })
 
 const store = createStore(reducer, 
-  composeWithDevTools());
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  ),
+);
 
 anecdoteService.getAll().then((anecdotes) => {
   store.dispatch(initializeAnecdotes(anecdotes));
