ec9f2c983dc305b2045f6fcb5cb1a61aa5fe92e2
pandu <lifeisubw@gmail.com>
2021-02-05 10:38:30 +0700

    Finish 8.6: Adding a book

--- Part8/library-backend/index.js
+++ Part8/library-backend/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { v1: uuid } = require('uuid');
 
 let findAuthorWritten = (authorName) => {
   let count = 0;
@@ -32,70 +33,65 @@ let authors = [
   },
 ]
 
-/*
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
-*/
-
 let books = [
   {
     title: 'Clean Code',
     published: 2008,
     author: 'Robert Martin',
+    genres: ['refactoring'],
     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
   },
   {
     title: 'Agile software development',
     published: 2002,
     author: 'Robert Martin',
+    genres: ['agile', 'patterns', 'design'],
     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
   },
   {
     title: 'Refactoring, edition 2',
     published: 2018,
     author: 'Martin Fowler',
+    genres: ['refactoring'],
     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
   },
   {
     title: 'Refactoring to patterns',
     published: 2008,
     author: 'Joshua Kerievsky',
+    genres: ['refactoring', 'patterns'],
     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
   },  
   {
     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
     published: 2012,
     author: 'Sandi Metz',
+    genres: ['refactoring', 'design'],
     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
   },
   {
     title: 'Crime and punishment',
     published: 1866,
     author: 'Fyodor Dostoevsky',
+    genres: ['classic', 'crime'],
     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
   },
   {
     title: 'The Demon ',
     published: 1872,
     author: 'Fyodor Dostoevsky',
+    genres: ['classic', 'revolution'],
     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
   },
 ]
 
 const typeDefs = gql`
   type Book {
     title: String!
-    author: String!
     published: Int!
-    genres: [String!]!
+    author: String!
     id: ID!
+    genres: [String!]!
   }
 
   type Author {
@@ -113,6 +109,15 @@ const typeDefs = gql`
       genre: String
     ): [Book!]!
     allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
   }
 `
 
@@ -151,6 +156,24 @@ const resolvers = {
         result.push(query);
       });
       return result;
+    }
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      if (authors.filter((author) => author.name === args.author).length > 0) {
+        const book = { ...args, id: uuid() };
+        books = books.concat(book);
+        return book;
+      } else {
+        const author = { name: args.author, 
+          born: null, bookCount: 1, id: uuid() };
+        authors = authors.concat(author);
+        const book = { ...args, id: uuid() };
+        books = books.concat(book);
+        console.log(books);
+        return book;
+      }
     }
   }
 }
